aeff0b3c377b08c632b186da0ad922f26d079933
acvt719 <georgios.karanasios@city.ac.uk>
2021-04-12 02:03:57 +0300

    Fixed the assigned user projects issue

--- mongodb_models/settings_mongo.py
+++ mongodb_models/settings_mongo.py
@@ -1,4 +1,5 @@
 import os
+from pymongo.errors import ConnectionFailure
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 
@@ -8,8 +9,10 @@ MONGO_URI = os.environ.get('MDB')
 
 
 def config_mongo_db_with_app(app):
-    app.debug = os.environ.get('DEBUG', True)
-    app.config["MONGO_URI"] = MONGO_URI
-    mongo_db = PyMongo(app)
-
-    return mongo_db
+    try:
+        app.debug = os.environ.get('DEBUG', True)
+        app.config["MONGO_URI"] = MONGO_URI
+        mongo_db = PyMongo(app)
+        return mongo_db
+    except ConnectionFailure as e:
+        print("Could not connect to server: %s" % e)
